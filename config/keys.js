c5aaa25934676a036deb3e0498df1e29a69e455a
hiteshLoke-122 <101446055+hiteshLoke-122@users.noreply.github.com>
2022-04-20 21:31:10 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://Hitesh123:'+ encodeURIComponent('12345') + '@new.qhkfj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
